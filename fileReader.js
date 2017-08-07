ffef17a45551627372d7ec7978a0b5b7efb6b9ea
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-07 16:16:01 -0400

    updates filereader.js

--- fileReader.js
+++ fileReader.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 const readline = require('readline');
+const dns = require('dns');
 
-const rl = readline.createInterface ({
-        input: process.stdin,
-        output: process.stdout
-});
+// const rl = readline.createInterface ({
+//         input: process.stdin,
+//         output: process.stdout
+// });
 
 class FileManipulator {
     constructor() {
 
     }
-    readFile(name) {
+    readFile(name, callback) {
+        fs.readFile(name, (err,buffer) => {
+            if (err) {
+                return err.message;
+            }
+            let content = buffer.toString();
+            console.log(content);
+            callback(content);
+        });
+    }
 
+    IpLookUp(domain, callback) {
+        dns.lookup(domain, (err, address, family) => {
+        if (err) {
+            return err.message;
+        }
+            console.log("IP address: " + address);
+        });
     }
 }
 
