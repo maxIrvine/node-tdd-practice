a7ac327786178f5208e3a0e6a2014ec3bd2cf6f5
Maxwellgit  Irvine <max.irvine99@gmail.com>
2017-08-07 16:16:06 -0400

    removes test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-const expect = require('chai').expect;
-
-let file = new FileManipulator();
-
-describe('Can read file', () => {
-    it('can read a file with string', () => {
-        expect(file("test.txt")).to.equal("hello max");
-    });
-});
